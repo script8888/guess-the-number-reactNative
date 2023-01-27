57ff8a8f64392147b2bafbb010780b56bec76818
Enoch Fabiyi <ayomikun204@gmail.com>
2023-01-27 23:09:38 +0100

    implemented SplashScreen package

--- App.js
+++ App.js
@@ -1,24 +1,78 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [rounds, setRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await fetchFonts();
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setDataLoaded(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (dataLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [dataLoaded]);
+
+  if (!dataLoaded) {
+    return null;
+  }
 
   const startGameHandler = (number) => {
     setUserNumber(number);
+    setRounds(0);
+  };
+
+  const startOverHandler = () => {
+    setRounds(0);
+    setUserNumber(null);
+  };
+
+  const gameOverHandler = (round) => {
+    setRounds(round);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
-
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />;
+  if (userNumber && rounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (rounds > 0) {
+    content = <GameOverScreen startOver={startOverHandler} round={rounds} />;
   }
   return (
-    <View style={styles.screen}>
+    <View onLayout={onLayoutRootView} style={styles.screen}>
       <Header title={"Guess a Number"} />
       {content}
 
