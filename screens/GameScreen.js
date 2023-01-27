789b93b372ff177ba5df0192531a58124ab13012
Enoch Fabiyi <ayomikun204@gmail.com>
2023-01-27 22:25:26 +0100

    more changes to game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useRef } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import Card from "../components/Card";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -18,8 +19,59 @@ function GameScreen(props) {
     generateRandomBetween(1, 100, props.userChoice)
   );
 
-  return <View style={styles.screen}></View>;
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("You fucking liar.", "Spazzy Bastard");
+      return;
+    }
+
+    if (direction === "lower") {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <Text>{currentGuess}</Text>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={nextGuessHandler.bind(this, "lower")} />
+        <Button
+          title="GREATER"
+          onPress={nextGuessHandler.bind(this, "greater")}
+        />
+      </Card>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({ screen: {} });
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
 export default GameScreen;
