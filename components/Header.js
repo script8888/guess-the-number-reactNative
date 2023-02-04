d54a28f71fc42dadb57abd21ceb500e7aefb9de1
Enoch Fabiyi <ayomikun204@gmail.com>
2023-02-04 23:31:57 +0100

    learn about Platform API

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Platform } from "react-native";
 import Colors from "../constants/colors";
 
 function Header(props) {
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primary,
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
--- components/MainButton.js
+++ components/MainButton.js
@@ -1,21 +1,38 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
 
 function MainButton(props) {
+  let TouchableComp = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableComp = TouchableNativeFeedback;
+  }
   return (
-    <TouchableOpacity onPress={props.onPress}>
-      <View style={{ ...styles.button, backgroundColor: props.color }}>
-        <Text style={styles.btnText}>{props.children}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      <TouchableComp activeOpacity={0.6} onPress={props.onPress}>
+        <View style={{ ...styles.button, backgroundColor: props.color }}>
+          <Text style={styles.btnText}>{props.children}</Text>
+        </View>
+      </TouchableComp>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 25,
+    overflow: "hidden",
+  },
   button: {
     paddingVertical: 12,
     paddingHorizontal: 15,
-    borderRadius: 25 
+    borderRadius: 25,
   },
   btnText: {
     color: "#fff",
