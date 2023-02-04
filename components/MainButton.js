c97a03dc4bfa8bf07233d1d5163c09df80eebc2f
Enoch Fabiyi <ayomikun204@gmail.com>
2023-02-04 16:30:54 +0100

    clean up

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import colors from "../constants/colors";
 
 function MainButton(props) {
   return (
